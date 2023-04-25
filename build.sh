11a23db1c288d91996712f0f1ac94bc55f46c105
pandemicpig <pandemicpig@gmail.com>
2023-04-25 10:16:08 +0100

    build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh
+
+if [ "$1" = "be" ]; then
+  cd /var/project/nomadwisdom-data-service
+  git fetch
+  git pull
+  git checkout master
+  cp -r /var/project/nomadwisdom-data-service/* /var/www/html/
+  echo "BE job done"
+else
+    if [ "$1" = "fe" ]; then
+      cd /var/project/nomadwisdom
+      git fetch
+      git pull
+      git checkout main
+      npm run build
+      cp -r /var/project/nomadwisdom/* /var/www/html/
+      echo "FE job done"
+    else
+      echo "nothing to deploy"
+    fi
+fi
